bff4f33266934628741a2f2db1c1a29617577284
ericswetts <eric.swetts@gmail.com>
2018-12-07 15:09:53 -0800

    Complete BenchBnB setup

---
+++ frontend/bench_bnb.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>heyyy react working</h1>, root);
+});
